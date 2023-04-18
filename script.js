452dc2f7d1edab3a4be0028ca69cdb142091c02e
Valais Girl <Yevette@Hal-71.local>
2023-04-18 11:19:10 -0500

    API call redundancy

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+// const form = document.querySelector('#gif-container');
+// const input = document.querySelector('#input');
 
-const form = document.querySelector('.pure-form');
-const input = document.querySelector('.pure-input-rounded');
 
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
 
     const DictionaryApiKey = '42f40c1e-656e-47a8-9597-6b3c3c5fdbe0';
     const query = "voluminous"
@@ -19,42 +18,81 @@ form.addEventListener('submit', (event) => {
         console.error(error);
       });
 
+// });
+
+// $(document).ready(function() {
+
+//   const giphyApiKey = 'DQQk3Czth43tzR6goSHYIQXrKreMrWf2';
+
+//   function fetchGifs() {
+//     const query = 'dogs'; //!
+//     if (query) {
+//       fetch(`https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${query}`)
+//       .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(data) {
+//         displayGifs(data.data);
+//         console.log(data);
+//       });
+//     }
+//   }  document.getElementById('clicky').addEventListener('click', fetchGifs);
 
 
-});
+//   function displayGifs(gifs) {
+//     const gifContainer = document.getElementById('gif-container');
+//     gifContainer.innerHTML = '';
 
-$(document).ready(function() {
+//     const topGifs = gifs.slice(0,10);
+//     const randomIndex = Math.floor(Math.random() * topGifs.length);
+//     const randomGif = topGifs[randomIndex];
 
-  const giphyApiKey = 'DQQk3Czth43tzR6goSHYIQXrKreMrWf2';
-
-  function fetchGifs() {
-    const query = 'dogs'; //!
-    if (query) {
-      fetch(`https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${query}`)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        displayGifs(data.data);
-        console.log(data);
-      });
-    }
-  }  document.getElementById('clicky').addEventListener('click', fetchGifs);
-
-
-  function displayGifs(gifs) {
-    const gifContainer = document.getElementById('gif-container');
-    gifContainer.innerHTML = '';
-
-    const topGifs = gifs.slice(0,10);
-    const randomIndex = Math.floor(Math.random() * topGifs.length);
-    const randomGif = topGifs[randomIndex];
-
-    const img = document.createElement('img');
-    img.src = randomGif.images.fixed_width.url;
-    img.classList.add('gif-item');
-    gifContainer.appendChild(img);
-  };
+//     const img = document.createElement('img');
+//     img.src = randomGif.images.fixed_width.url;
+//     img.classList.add('gif-item');
+//     gifContainer.appendChild(img);
+//   };
 
   
-});
+// });
+
+// const DictionaryApiKey = '42f40c1e-656e-47a8-9597-6b3c3c5fdbe0';
+// const giphyApiKey = 'DQQk3Czth43tzR6goSHYIQXrKreMrWf2';
+
+// const form = document.querySelector('#gif-container');
+// const input = document.querySelector('#input');
+
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+  
+//   const query = input.value;
+  
+//   // Fetch data from Dictionary API
+//   fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${query}?key=${DictionaryApiKey}`)
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data); // Do something with the dictionary data
+      
+//       // Navigate to the second page and pass the dictionary data as a URL parameter
+//       window.location.href = `results.html?dictionaryData=${encodeURIComponent(JSON.stringify(data))}`;
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
+  
+//   // Fetch data from GIPHY API
+//   fetch(`https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${query}`)
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data); // Do something with the GIPHY data
+      
+//       // Store GIPHY data in local storage
+//       localStorage.setItem('giphyData', JSON.stringify(data));
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
+// });
+
+
+
